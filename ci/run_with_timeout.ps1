06e3c6af4cc371f4c5967f8198e0762263689e62
Christian Flach <cmfcmf.flach@gmail.com>
2018-03-30 22:00:28 +0200

    correct things

--- ci/run_with_timeout.ps1
+++ ci/run_with_timeout.ps1
@@ -2,9 +2,6 @@
 
 $proc = Start-Process -filePath $program -ArgumentList $arguments -PassThru
 
-# keep track of timeout event
-$timeouted = $null # reset any previously set timeout
-
 # wait up to x seconds for normal termination
 $proc | Wait-Process -Timeout $timeout -ea 0 -ev timeouted
 
